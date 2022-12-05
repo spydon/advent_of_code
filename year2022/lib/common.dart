200eeeddef18bc9e98b22841b7cc6a5698ac35ac
Lukas Klingsbo <me@lukas.fyi>
2022-12-05 23:00:21 +0100

    Add some common list methods

---
+++ year2022/lib/common.dart
@@ -0,0 +1,27 @@
+import 'package:collection/collection.dart';
+
+extension ListExtension<T> on List<T> {
+  List<T> takeLast(int i) {
+    return sublist(length - i, length);
+  }
+
+  List<T> removeBack(int i) {
+    final result = sublist(length - i, length);
+    removeRange(length - i, length);
+    return result;
+  }
+
+  List<T> removeFront(int i) {
+    final result = take(i).toList();
+    removeRange(0, i);
+    return result;
+  }
+}
+
+void main() {
+  print([1, 2, 3, 4].takeLast(2));
+  final test = [1, 2, 3, 4];
+  print(test.removeBack(2));
+  print(test);
+  print([1, 2, 3, 4].removeFront(2));
+}
