342f688fe6f331b0cfba51af281d412ba2d24fe1
Lukas Klingsbo <SEFUNCTeamRatatosk@skandia.se>
2022-12-06 06:21:05 +0100

    Add file for day 6 solution 2

--- year2022/lib/6-2.dart
+++ year2022/lib/6-2.dart
@@ -4,13 +4,13 @@ import 'dart:io';
 //import 'common.dart';
 
 void main() {
-  final window = List.generate(4, (_) => '');
+  final window = List.generate(14, (_) => '');
   var pos = 0;
   final input = File('6-1.txt').readAsStringSync().split('');
   for (final c in input) {
-    window[pos % 4] = c;
+    window[pos % 14] = c;
     pos++;
-    if (window.toSet().length == 4 && !window.contains('')) {
+    if (window.toSet().length == 14 && !window.contains('')) {
       print(pos);
       break;
     }
