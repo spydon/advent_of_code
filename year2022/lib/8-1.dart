053c8649cf1b0009d0532589c1a602ac91d114bb
Lukas Klingsbo <SEFUNCTeamRatatosk@skandia.se>
2022-12-08 09:30:38 +0100

    Day 8

---
+++ year2022/lib/8-1.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:collection_ext/all.dart';
+import 'package:year2022/common.dart';
+//import 'common.dart';
+
+void main() {
+  final input = File('8-1.txt')
+      .readAsStringSync()
+      .split('\n')
+      .map((l) => l.split('').map(int.parse).toList())
+      .toList();
+  int visible = 0;
+  for (var x = 1; x < input[x].length - 1; x++) {
+    for (var y = 1; y < column(y, input).length - 1; y++) {
+      final xAxis = input[x];
+      final xSplit = [
+        xAxis.take(y).toList(),
+        xAxis.takeLast(input[y].length - y - 1).toList()
+      ];
+      final yAxis = column(y, input);
+      final ySplit = [
+        yAxis.take(x).toList(),
+        yAxis.takeLast(input[x].length - x - 1).toList()
+      ];
+      final left = xSplit[0];
+      final right = xSplit[1];
+      final top = ySplit[0];
+      final bottom = ySplit[1];
+      final tree = input[x][y];
+      if ([left, right, top, bottom].any((e) => treeVisible(tree, e))) {
+        visible++;
+      }
+    }
+  }
+  print(visible + (input.length - 2) * 2 + input[0].length * 2);
+}
+
+bool treeVisible(int height, List<int> trees) {
+  return trees.all((t) => t < height);
+}
+
+List<int> column(int y, List<List<int>> input) {
+  final result = <int>[];
+  for (var x = 0; x < input.length; x++) {
+    result.add(input[x][y]);
+  }
+  return result;
+}
---
+++ year2022/lib/8-2.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:collection_ext/all.dart';
+import 'package:year2022/common.dart';
+//import 'common.dart';
+
+void main() {
+  final input = File('8-1.txt')
+      .readAsStringSync()
+      .split('\n')
+      .map((l) => l.split('').map(int.parse).toList())
+      .toList();
+  int score = 1;
+  for (var x = 1; x < input[x].length - 1; x++) {
+    for (var y = 1; y < column(y, input).length - 1; y++) {
+      final xAxis = input[x];
+      final yAxis = column(y, input);
+      final xSplit = [
+        xAxis.take(y).toList(),
+        xAxis.takeLast(input[y].length - y - 1).toList()
+      ];
+      final ySplit = [
+        yAxis.take(x).toList(),
+        yAxis.takeLast(input[x].length - x - 1).toList()
+      ];
+      final left = xSplit[0];
+      final right = xSplit[1];
+      final top = ySplit[0];
+      final bottom = ySplit[1];
+      final tree = input[x][y];
+      int currentScore = 1;
+      for (final l in [left.reversed, right, top.reversed, bottom]) {
+        int j = 0;
+        for (final i in l) {
+          j++;
+          if (i >= tree || l.length == j) {
+            currentScore *= j;
+            break;
+          }
+        }
+      }
+      score = max(score, currentScore);
+    }
+  }
+  print('Score: $score');
+}
+
+bool treeVisible(int height, List<int> trees) {
+  return trees.all((t) => t < height);
+}
+
+List<int> column(int y, List<List<int>> input) {
+  final result = <int>[];
+  for (var x = 0; x < input.length; x++) {
+    result.add(input[x][y]);
+  }
+  return result;
+}
