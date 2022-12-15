1dc00fd154ae7fc1f594f590ef124a783d5aad27
Lukas Klingsbo <me@lukas.fyi>
2022-12-15 21:05:51 +0100

    Day 15 - 2 million is not 4 million 😠

--- year2022/lib/15-2.dart
+++ year2022/lib/15-2.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:collection_ext/all.dart';
+
 void main() {
   final lines = File('15-1.txt')
       .readAsStringSync()
@@ -11,46 +13,44 @@ void main() {
       .split('\n')
       .map((l) {
     final nums = l.split(' ');
-    return [Pair(nums[0], nums[1]), Pair(nums[2], nums[3])];
+    return [
+      Pair.fromString(nums[0], nums[1]),
+      Pair.fromString(nums[2], nums[3]),
+    ];
   });
 
-  for (var row = 0; row < 2000000; row++) {
-    print(row);
-    final covers = <int>{};
-    for (final pairs in lines) {
-      final d = pairs[0].distanceTo(pairs[1]);
-      if (pairs[0].y + d < row || pairs[0].y - d > row) {
+  final m = <Pair, int>{};
+  for (final pairs in lines) {
+    final d = pairs[0].distanceTo(pairs[1]);
+    m[pairs[0]] = d;
+  }
+
+  for (int y = 0; y <= 2000000; y++) {
+    print(y);
+    for (int x = 0; x <= 2000000; x++) {
+      if (m.any((p0, dOther) => p0.distance(x, y) < dOther)) {
         continue;
       }
-      final toRow = (pairs[0].y - row).abs();
-      for (int i = 0; i < d - toRow + 1; i++) {
-        final left = pairs[0].x - i;
-        final right = pairs[0].x + i;
-        if (left >= 0 && left <= 4000000) {
-          covers.add(left);
-        }
-        if (right >= 0 && right <= 4000000) {
-          covers.add(right);
-        }
-      }
-    }
-    if (covers.length != 4000001) {
-      print('Row found $row ${covers.length}');
-      break;
+      print('(x, y)');
     }
   }
 }
 
 class Pair {
-  Pair(String sx, String sy) {
-    x = int.parse(sx);
-    y = int.parse(sy);
+  const Pair(this.x, this.y);
+
+  factory Pair.fromString(String sx, String sy) {
+    return Pair(int.parse(sx), int.parse(sy));
   }
 
-  late final int x, y;
+  final int x, y;
 
   int distanceTo(Pair other) {
     return (other.x - x).abs() + (other.y - y).abs();
+  }
+
+  int distance(int xOther, int yOther) {
+    return (xOther - x).abs() + (yOther - y).abs();
   }
 
   @override
