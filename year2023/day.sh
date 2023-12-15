052a076740d9c30acecdea5e72ef40d1008c24ca
jonathan <sharyari@gmail.com>
2023-12-15 07:22:27 +0100

    Update template

--- year2023/day.sh
+++ year2023/day.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 aocd 2023 $1 > $1.txt
-cp lib/day_1.dart lib/day_$1.dart
+cp lib/template.dart lib/day_$1.dart
+sed -i "s/100/$1/g" lib/day_$1.dart
 echo "Day $1 setup complete, good luck!"
